f83e164dffc05438f07982a9f875ae6447e6492b
Grégory D'Angelo <dangelo.gregory@gmail.com>
2015-10-16 09:31:54 +0200

    Render topics list using fetch

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
 var Routes = require('./routes');
 
+var Api = require('./utils/api');
+
 React.render(Routes, document.querySelector('.container'));
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -2,14 +2,23 @@ var React = require('react');
 var ReactRouter = require('react-router');
 
 var Header = require('./header');
+var TopicList = require('./topic-list');
 
 module.exports = React.createClass({
   render: function () {
     return (
       <div>
         <Header />
-        {this.props.children}
+        {this.content()}
       </div>
     );
+  },
+
+  content: function () {
+    if (this.props.children) {
+      return this.props.children;
+    } else {
+      return <TopicList />;
+    }
   }
 });
