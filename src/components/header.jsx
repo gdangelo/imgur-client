7614fde88c467c134cd004e4dd4cd345e75f8d51
gdangelo <dangelo.gregory@gmail.com>
2015-10-17 16:13:26 +0200

    Render topics list in hte header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,22 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 var Link = ReactRouter.Link;
+var Reflux = require('reflux');
+var TopicStore = require('../stores/topic-store');
+var Actions = require('../actions');
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+  getInitialState: function () {
+    return {
+      topics: []
+    }
+  },
+  componentWillMount: function () {
+    Actions.getTopics();
+  },
   render: function () {
     return (
       <nav className="navbar navbar-default">
@@ -10,11 +24,30 @@ module.exports = React.createClass({
           <div className="navbar-header">
             <Link to="/" className="navbar-brand">Imgur-client</Link>
           </div>
-          <ul className="nav navbar-nav navbar-right">
-            <li><Link to="/1">Topic #1</Link></li>
-          </ul>
+          <div id="navbar" className="navbar-collapse collapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li className="dropdown">
+                <a className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span className="caret"></span></a>
+                <ul className="dropdown-menu">
+                  {this.renderTopics()}
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
+  },
+  renderTopics: function () {
+    return this.state.topics.map(function(topic){
+      return <li key={topic.id}>
+        <Link to={"/topics/" + topic.id}>{topic.name}</Link>
+      </li>
+    });
+  },
+  onChange: function (event, topics) {
+    this.setState({
+      topics: topics
+    });
   }
 });
