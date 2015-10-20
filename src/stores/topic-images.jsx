f0c0dc2de3f037c35c6adacc4127543af0942e78
Grégory D'Angelo <dangelo.gregory@gmail.com>
2015-10-20 10:08:11 +0200

    Filter image data

--- src/stores/topic-images.jsx
+++ src/stores/topic-images.jsx
@@ -1,13 +1,16 @@
 var Reflux = require('reflux');
 var Api = require('../utils/api');
 var Actions = require('../actions');
+var _ = require('lodash');
 
 module.exports = Reflux.createStore({
     listenables: [Actions],
     getImages: function (id) {
       return Api.get('topics/' + id)
         .then(function(json){
-          this.images = json.data;
+          this.images = _.reject(json.data, function(image){
+            return image.is_album; // Exclude topic images which are albums
+          });
           this.triggerChange();
         }.bind(this));
     },
