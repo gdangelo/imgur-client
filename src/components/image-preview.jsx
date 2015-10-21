409ed5484cf2045b30a25e0a4c4581cefa9f3bc6
Grégory D'Angelo <dangelo.gregory@gmail.com>
2015-10-21 10:14:03 +0200

    Handle video preview on mouse hover

--- src/components/image-preview.jsx
+++ src/components/image-preview.jsx
@@ -1,14 +1,34 @@
 var React = require('react');
 
 module.exports = React.createClass({
+    getInitialState: function () {
+      return {
+        hovering: false
+      };
+    },
     render: function () {
-      return <div className="image-preview">
-        {this.images()}
+      return <div
+        className="image-preview"
+        onMouseEnter={this.handleMouseEnter}
+        onMouseLeave={this.handleMouseLeave}>
+
+        {this.state.hovering && this.props.animated ? this.videos() : this.images()}
       </div>
     },
     images: function () {
       var link = "https://i.imgur.com/" + this.props.id + "h.jpg";
 
       return <img src={link} />;
+    },
+    videos: function () {
+      return <video autoPlay="autoplay" loop="loop" preload="auto">
+        <source src={this.props.mp4} type="video/mp4"></source>
+      </video>;
+    },
+    handleMouseEnter: function () {
+      this.setState({hovering: true});
+    },
+    handleMouseLeave: function () {
+      this.setState({hovering: false});
     }
 });
